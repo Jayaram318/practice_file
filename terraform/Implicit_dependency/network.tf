ba4c41761da0f30ef4b7e414e1dad1ce3cd29ee2
Jayaram318 <jayaramireddy318@gmail.com>
2023-01-16 21:43:51 +0530

    add changes

--- terraform/Implicit_dependency/network.tf
+++ terraform/Implicit_dependency/network.tf
@@ -1,28 +1,28 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.10.0.0/16"
-     tags = {
-       "Name" = "tff"
+  cidr_block       = "10.10.0.0/16"
+     tags          = {
+       "Name"      = "tff"
      }
 }
 resource "aws_subnet" "subnet_1" {
-    vpc_id = aws_vpc.my_vpc.id
-    cidr_block = "10.10.0.0/24"
+    vpc_id         = aws_vpc.my_vpc.id
+    cidr_block     = "10.10.0.0/24"
     tags = {
-      "Name" = "frntend"
+      "Name"       = "frntend"
     }
   
 }
 resource "aws_subnet" "subnet_2" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.10.1.0/24"
-  tags = {
-    "Name" = "database"
+  vpc_id          = aws_vpc.my_vpc.id
+  cidr_block      = "10.10.1.0/24"
+  tags            = {
+    "Name"        = "database"
   }
 }
 resource "aws_subnet" "subnet_3" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.10.2.0/24"
-  tags = {
-    "Name" = "middleware"
+  vpc_id         = aws_vpc.my_vpc.id
+  cidr_block     = "10.10.2.0/24"
+  tags           = {
+    "Name"       = "middleware"
   }
 }
